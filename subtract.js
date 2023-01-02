0da88fa9f007eb51e806f47ddb00415bd9109e70
jyotijo <jyotisoratur@gmail.com>
2023-01-02 11:12:45 +0530

    js file is added

--- subtract.js
+++ subtract.js
@@ -1,1 +1,8 @@
-this is sub file
+function subtract() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result = a-b
+document.getElementById("result").value=result
+}
+
